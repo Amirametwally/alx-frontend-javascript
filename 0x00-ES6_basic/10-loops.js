edb73d8858fe75d0e7a16ef6278db9bc894537fe
Amirametwally <amira.metwally56@gmail.com>
2024-05-28 00:14:34 +0300

    10. For...of Loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const new_array = [];
+  const newArray = [];
   // eslint-disable-next-line no-restricted-syntax
   for (const value of array) {
-    new_array.push(appendString + value);
+    newArray.push(appendString + value);
   }
-  return new_array;
+
+  return newArray;
 }
